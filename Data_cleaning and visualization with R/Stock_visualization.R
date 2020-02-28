4e89fe1cd9f31c3bd101d4a43304f3f238dc19b2
AMON <akouass@ncsu.edu>
2020-02-27 19:35:01 -0500

    Update Stock_visualization.R

--- Data_cleaning and visualization with R/Stock_visualization.R
+++ Data_cleaning and visualization with R/Stock_visualization.R
@@ -5,12 +5,15 @@
 # Clear the console
 cat("\f")
 
-# Get quantmod and magrittr 
+# Get Packages
 install.packages("quantmod")
 install.packages("magrittr")
+install.packages("ggplot2")
+install.packages("ggcorrplot")
+
 # Import the library
 library(quantmod)
-library(magrittr) # for the log change 
+
 
 # Stocks Datas over 1 year period
 start_date <- as.Date("2019-01-30")
@@ -28,6 +31,8 @@ stocks<- as.xts(data.frame(AAPL = AAPL[, "AAPL.Close"], MSFT = MSFT[, "MSFT.Clos
                            AMZN = AMZN[, "AMZN.Close"], FB=FB[, "FB.Close"]))
 head(stocks, n=10L)
 
+library(magrittr) # for the log change 
+
 #  The log differences 
 stock_change = stocks %>% log %>% diff
 head(stock_change)
@@ -42,6 +47,24 @@ plot(as.zoo(stock_change),screens = 1,  lty = 1:3, xlab = "Date", ylab = "Log Di
 legend("topleft", c("AAPL", "MSFT", "AMZN", "FB"), lty = 1:3, cex = 0.5)
 
 
-# Correlation matrix
-correlations <- cor(as.matrix(stock_return ), method="spearman")
-correlations 
+library(ggcorrplot) # to visualize correlation matrix
+
+
+# create a dataframe, corr, to hold the correlation matrix, rounded to two digits
+correlations_matrix = round(cor(stock_return),2)
+
+# Plot the correlation matrix 
+ggcorrplot(correlations_matrix, hc.order = TRUE, type ="lower", 
+           outline.color = "white")
+# Add the lab=TRUE parameter to show the correlation values
+ggcorrplot(correlations_matrix, hc.order = TRUE, type = "lower",
+           outline.col = "white", lab=TRUE)
+
+# create a dataframe, p.mat to contain the p values of the correlations
+p.mat <- cor_pmat(stock_return)
+
+# The color of the data values is also updated to white to mask out zero values
+ggcorrplot(correlations_matrix, p.mat = p.mat, hc.order = TRUE,
+           type = "lower", insig = "blank", lab=TRUE, lab_col="white")
+
+       
